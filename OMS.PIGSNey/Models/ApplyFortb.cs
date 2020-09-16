ec7bd9e811b4523aab56e49cc03ba04ac42dd96c
YUANSHAOFENG <YUANSHAOFENG@DESKTOP-85I5BGA>
2020-09-16 14:15:05 +0800

    1

--- OMS.PIGSNey/Models/ApplyFortb.cs
+++ OMS.PIGSNey/Models/ApplyFortb.cs
@@ -7,6 +7,7 @@ namespace OMS.PIGSNey.Models
 {
    public class ApplyFortb
     {
+        public int AId { get; set; }
         public string MaterialName { get; set; }
         public int UId { get; set; }
         public int MaterialAmount { get; set; }
