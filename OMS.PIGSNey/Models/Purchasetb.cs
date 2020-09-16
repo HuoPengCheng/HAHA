9fd43dacd18c07bc14bff77ca7bcd9450e565f08
郑明辰 <@ZHENGMINGCHEN>
2020-09-16 15:21:16 +0800

    a

--- OMS.PIGSNey/Models/Purchasetb.cs
+++ OMS.PIGSNey/Models/Purchasetb.cs
@@ -7,6 +7,7 @@ namespace OMS.PIGSNey.Models
 {
    public class Purchasetb
     {
+        //123
         public string MAterialName { get; set; }
         public string Category { get; set; }
         public int PAmount { get; set; }
