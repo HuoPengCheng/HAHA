a9e164ad5746e6e5a560f091705917c05395089e
YUANSHAOFENG <YUANSHAOFENG@DESKTOP-85I5BGA>
2020-09-16 18:55:11 +0800

    1

---
+++ OMS.PIGSNey/Controllers/TheorderdetailsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+/// <summary>
+/// 袁少峰  订单详情表
+/// </summary>
+namespace OMS.PIGSNey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TheorderdetailsController : ControllerBase
+    {
+    }
+}
