6029dfe450b0043de7c5c2eaefb27e2f8f141dfd
YUANSHAOFENG <YUANSHAOFENG@DESKTOP-85I5BGA>
2020-09-16 15:32:49 +0800

    a

--- OMS.PIGSNey/Models/Purchasetb.cs
+++ OMS.PIGSNey/Models/Purchasetb.cs
@@ -8,7 +8,7 @@ namespace OMS.PIGSNey.Models
    public class Purchasetb
     {
         //123
-        //123
+        //123123
         //123
         public string MAterialName { get; set; }
         public string Category { get; set; }
