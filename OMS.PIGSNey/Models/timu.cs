f592f8c1fe49c583fa7c6694397b097fffcdb13e
家旺 张 <zjw991228@outlook.com>
2020-09-22 08:58:25 +0800

    991228

--- OMS.PIGSNey/Models/timu.cs
+++ OMS.PIGSNey/Models/timu.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OMS.PIGSNey.Models
 {
+    [Table("timu")]
     public class timu
     {
         [Key]
